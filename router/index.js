2c40c53d78ca033a5fc14cc798efc556edf89d95
Omadbek <shustrbola@gmail.com>
2025-04-22 19:57:18 +0500

    buldi

--- router/index.js
+++ router/index.js
@@ -4,8 +4,8 @@ const router = Router()
 
 router
     .get("/users/all", controller.GETALL)
-    .get("/users/{userId}/balance", controller.GET)
-    .get("/transactions/history/{userId}", controller.GETHISTORY)
+    .get("/users/:id", controller.GET)
+    .get("/transactions/history/:id", controller.GETHISTORY)
     .post("/transactions/transfer", controller.POST)
     .put("/users/{userId}/limit", controller.PUT)
 export default router
